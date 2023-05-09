4bedb5ad7df6ccbcc93ad1582cf69f71ecac361b
TylerNDev <127581539+TylerNDev@users.noreply.github.com>
2023-05-08 19:03:55 -0500

    Add files via upload

---
+++ hw4/localStorage.js
@@ -0,0 +1,64 @@
+// function displayInputFromLocalStorage() {
+//   var formData = JSON.parse(localStorage.getItem("formData"));
+//   var formoutput = "<table>";
+//   for (var key in formData) {
+//     formoutput += "<tr><td>" + key + "</td><td>" + formData[key] + "</td></tr>";
+//   }
+//   formoutput += "</table>";
+//   document.getElementById("userinput").innerHTML = formoutput;
+// }
+
+// function submitForm() {
+//   var formcontents = document.getElementById("signup");
+//   var formData = {};
+//   for (var i = 0; i < formcontents.length; i++) {
+//     var element = formcontents.elements[i];
+//     if (element.value !== "") {
+//       if (element.type === "checkbox") {
+//         formData[element.name] = element.checked;
+//       } else {
+//         formData[element.name] = element.value;
+//       }
+//     }
+//   }
+//   localStorage.setItem("formData", JSON.stringify(formData));
+//   console.log(formData);
+//   window.location.href = "thanks.html";
+// }
+
+function storeInput() {
+  var formcontents = document.getElementById("signup");
+  var data = {};
+  for (var i = 0; i < formcontents.length; i++) {
+    var element = formcontents.elements[i];
+    if (
+      element.value !== "" &&
+      element.type !== "submit" &&
+      element.type !== "reset" &&
+      element.type !== "button"
+    ) {
+      if (element.type === "checkbox") {
+        data[element.name] = element.checked;
+      } else if (element.type === "radio" && element.checked) {
+        data[element.name] = element.value;
+      } else {
+        data[element.name] = element.value;
+      }
+    }
+  }
+  localStorage.setItem("formData", JSON.stringify(data));
+}
+
+function displayInput() {
+  var formData = JSON.parse(localStorage.getItem("formData"));
+  if (formData) {
+    var formOutput =
+      "<table class='output'><th colspan='3'> Your Information</th>";
+    for (var key in formData) {
+      formOutput += "<tr><td align='right'>" + key + "</td>";
+      formOutput += "<td class='outputdata'>" + formData[key] + "</td></tr>";
+    }
+    formOutput += "</table>";
+    document.getElementById("output").innerHTML = formOutput;
+  }
+}
